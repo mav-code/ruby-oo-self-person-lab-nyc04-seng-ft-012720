7c254674e2309e340c2fbdd5abea8b8f2fe669d6
mav-code <mavisansmembres@gmail.com>
2020-01-29 15:50:42 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -63,28 +63,24 @@ class Person
 
     def take_bath
         self.hygiene=(@hygiene + 4)
-        if @hygiene > 10
-            @hygiene= 10
+
         end
         return "♪ Rub-a-dub just relaxing in the tub ♫" 
     end 
 
     def work_out
         self.hygiene=(@hygiene -3)
-        if @hygiene < 0
-            @hygiene= 0
+
         end
 
         self.happiness=(@happiness +2)
-        if @happiness > 10 
-            @happiness = 10 
+
         end 
         return "♪ another one bites the dust ♫"
     end 
     def call_friend(friend)
         self.happiness=(@happiness +3)
-        if @happiness > 10 
-            @happiness = 10 
+
         end 
     friend.happiness = friend.happiness + 3
     if friend.happiness > 10
@@ -96,8 +92,7 @@ class Person
     def start_conversation(friend, topic)
         if topic == "politics"
             self.happiness = (@happiness - 2)
-            if @happiness < 0 
-                @happiness = 0
+
             end 
             friend.happiness = friend.happiness - 2
             if friend.happiness < 0
@@ -106,8 +101,7 @@ class Person
             return 'blah blah partisan blah lobbyist'
         elsif topic == "weather"
             self.happiness = (@happiness + 1)
-            if @happiness > 10 
-                @happiness = 10
+
             end 
             friend.happiness = friend.happiness + 1
             if friend.happiness > 10
